18320ec0552b4205018856f3418d3fdd5b4a2094
kwonyoungwoo <63556795+kwonyoungwoo@users.noreply.github.com>
2023-01-21 14:59:12 +0900

    [Bronze IV] Title: 주사위 세개, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.py
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -9,10 +9,5 @@ elif(a!=b and b==c and a!=c):
 elif(a==c and a!=b and b!=c):
     prize=1000+(a*100)
 else:
-    if(a>b and a>c):
-        prize=a*100
-    elif(b>a and b>c):
-        prize=b*100
-    else:
-        prize+=c*100
+    prize=max(a,b,c)*100
 print(prize)
