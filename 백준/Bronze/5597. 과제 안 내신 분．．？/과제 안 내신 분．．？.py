e9f0a05483cc96e263f99e30d75fbae4f27262e9
kwonyoungwoo <63556795+kwonyoungwoo@users.noreply.github.com>
2023-01-27 01:32:22 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
@@ -1,8 +1,4 @@
-a=[]
-num=list(range(1,31))
-for i in range(28):
-    a.append(int(input()))
-    num[a[i]-1]=0
-for i in range(len(num)):
-    if num[i]!=0:
-        print(num[i])
+students = [int(input()) for i in range(28)]
+for i in range(1, 31):
+  	if i not in students:
+    		print(i)
