90bd9a5ca82a602ab58e607f2c7fbf283e2b97fd
kwonyoungwoo <63556795+kwonyoungwoo@users.noreply.github.com>
2023-01-29 02:45:27 +0900

    [Bronze II] Title: 나머지, Time: 32 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.py
+++ 백준/Bronze/3052. 나머지/나머지.py
@@ -1,9 +1,6 @@
 a=[]
-a_cnt=[]
 for i in range(10):
     n=int(input())
-    a.append(n)
-    a[i]=n%42
-    if a[i] not in a_cnt:
-        a_cnt.append(a[i])
-print(len(a_cnt))
+    if n%42 not in a:
+        a.append(n%42)
+print(len(a))
