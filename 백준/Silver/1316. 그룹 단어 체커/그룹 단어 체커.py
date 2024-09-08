59444bc6ff12a7499cd73b060a2960193080f2ff
Youngwoo Kwon <63556795+kwonup@users.noreply.github.com>
2024-09-08 15:25:02 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -1,14 +1,9 @@
 n = int(input())
-group_cnt = 0
+notGroup = 0
 for i in range(n):
     word = input()
-    isGroup = 1
-    for j in range(0,len(word)-1):
-        if word[j] == word[j+1]:
-            isGroup = 1
-        elif word[j] in word[j+1:]:
-            isGroup = 0
+    for j in range(len(word)-1):
+        if (word[j] != word[j+1]) and (word[j] in word[j+1:]) :
+            notGroup+=1
             break
-    if(isGroup==1): group_cnt += 1
-
-print(group_cnt)
+print(n-notGroup)
