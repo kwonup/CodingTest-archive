0e8258db7285cff300445b4b48a4e84882427d7c
Youngwoo Kwon <63556795+kwonup@users.noreply.github.com>
2025-04-21 20:40:32 +0900

    [Silver IV] Title: 인사성 밝은 곰곰이, Time: 76 ms, Memory: 43680 KB -BaekjoonHub

--- 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.py
+++ 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.py
@@ -1,16 +1,15 @@
 import sys 
 input = sys.stdin.readline
 n = int(input())
-isNew = False
 cnt=0
 user = set() #채팅하는 인원을 담아두는 집합(중복없애기위해 집합사용)
 for i in range(n):
     s = input().rstrip()
     if s == 'ENTER':
         cnt+= len(user)
-        user= set()
+        user.clear() #집합을 초기화
         continue
     user.add(s)
 
-cnt+=len(user)
+cnt += len(user) #마지막에 최종적으로 집합의 크기를 cnt변수에 더함
 print(cnt)
