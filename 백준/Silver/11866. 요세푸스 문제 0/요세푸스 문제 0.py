8b8b77320b05301572842adc61f3a0860de7149c
Youngwoo Kwon <63556795+kwonup@users.noreply.github.com>
2025-04-28 14:05:13 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -1,22 +1,13 @@
 import sys
 input = sys.stdin.readline
-from collections import deque
 
 n,k = map(int,input().split())
 circle = list(range(1,n+1))
 josephus = []
 idx = k-1
-for i in range(n):
-    #print('idx = ',idx)
+while circle:
     josephus.append(circle.pop(idx))
-    #print('josephus : ',josephus)
+    if not circle: break #마지막 순회때 len(circle)==0 이면 반복문 탈출
+    idx = (idx + k-1) % len(circle) #나머지 연산 사용 (원순열)
 
-    idx -= 1
-    for j in range(k):
-        idx += 1
-        if idx >= len(circle): 
-            idx = 0
-
-    #print(circle)
-    #print()
-print(f"<{', '.join(map(str, josephus))}>")
+print(f"<{', '.join(map(str,josephus))}>")
