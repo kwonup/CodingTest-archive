17eb85630040da8062c8340b1f971e34aa27e106
Youngwoo Kwon <63556795+kwonup@users.noreply.github.com>
2025-05-04 20:13:07 +0900

    [Silver IV] Title: 가로수, Time: 100 ms, Memory: 40120 KB -BaekjoonHub

--- 백준/Silver/2485. 가로수/가로수.py
+++ 백준/Silver/2485. 가로수/가로수.py
@@ -1,24 +1,22 @@
 import sys
 input = sys.stdin.readline
 n = int(input())
-def gcd(a,b): #유클리드 호제법으로 최소공배수 구하는 함수
+
+##최대공약수(gcd) 구하는 함수(유클리드 호제법 사용)
+def gcd(a,b):
     while b:
         a,b = b,a%b
     return a
 
-gaps=[]
-nums=[]
-tmp = 0
-for _ in range(n):
-    num = int(input())
-    nums.append(num)
-    gaps.append(num-tmp)
-    tmp = num
-gaps = gaps[1:]
+trees = [int(input()) for _ in range(n)] #가로수 입력받기
 
+gaps = [trees[i+1]-trees[i] for i in range(len(trees)-1)]#가로수의 간격들 저장하는 리스트
+
+#간격들의 최대공약수 구하는 로직
 gcd_all = gaps[0]
-for i in gaps[1:]:
+for i in gaps:
     gcd_all = gcd(gcd_all,i)
-        
+
+#간격들 사이에 들어갈 가로수개수
 answer = sum((i//gcd_all -1) for i in gaps)
 print(answer)
