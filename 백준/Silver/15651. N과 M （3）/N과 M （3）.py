7f04ad30cd8bd7aa2c877956a1f9ad144092afcb
Youngwoo Kwon <63556795+kwonup@users.noreply.github.com>
2025-06-04 21:13:55 +0900

    [Silver III] Title: N과 M (3), Time: 1100 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,11 @@
+n,m = map(int,input().split())
+answer = []
+def back():
+    if len(answer) == m:
+        print(" ".join(map(str,answer)))
+        return
+    for i in range(1,n+1):
+        answer.append(i)
+        back()
+        answer.pop()
+back()
