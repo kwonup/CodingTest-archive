f0e5e226c2217862436ae47d53baf006132ed5d0
Youngwoo Kwon <63556795+kwonup@users.noreply.github.com>
2025-07-05 20:14:35 +0900

    [Gold V] Title: 회의실 배정, Time: 244 ms, Memory: 60360 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.py
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.py
@@ -4,12 +4,11 @@ n = int(input())
 meeting = list(list(map(int,input().split())) for _ in range(n))
 
 meeting.sort(key = lambda x:(x[1],x[0])) # key를 기준으로 정렬
+
 cnt = 0
-available = []
-available.append(meeting[0])
-j=0
-for i in range(1,n):
-    if available[j][1]<=meeting[i][0]:
-        available.append(meeting[i])
-        j+=1
-print(len(available))
+endPoint = 0
+for start,end in meeting:
+    if endPoint <= start:
+        cnt+=1
+        endPoint = end
+print(cnt)
