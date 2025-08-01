3853720ac3d60449646f0136029a0483675ba330
Youngwoo Kwon <63556795+kwonup@users.noreply.github.com>
2025-08-01 21:53:32 +0900

    [Silver III] Title: 주유소, Time: 104 ms, Memory: 48604 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -1,10 +1,15 @@
 import sys
 input = sys.stdin.readline
+
 n = int(input())
 distance = list(map(int,input().split()))
 price = list((map(int,input().split())))
+
 total = 0
+min_price = price[0]
 for i in range(n-1):
-    m = min(price[:i+1])
-    total += m * distance[i]
+    if price[i]< min_price:
+        min_price = price[i]
+    total += min_price * distance[i]
+
 print(total)
