d1a76f93d0b9b1099877db34b0d983cc2c4c9ae6
Youngwoo Kwon <63556795+kwonup@users.noreply.github.com>
2025-09-01 19:24:39 +0900

    [level 2] Title: 더 맵게, Time: 1803.41 ms, Memory: 49.1 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -6,10 +6,9 @@ def solution(scv, K):
         min_1 = heapq.heappop(scv)
         if min_1 >= K:
             return cnt
-        else:
-            min_2 = heapq.heappop(scv)
-            heapq.heappush(scv, min_1 + (min_2*2))
-            cnt += 1
+        min_2 = heapq.heappop(scv)
+        heapq.heappush(scv, min_1 + (min_2*2))
+        cnt += 1
     #만약 모든음식 스코빌지수를 K이상으로 만들수없을경우
     if scv[0]<K: return -1
     return cnt
