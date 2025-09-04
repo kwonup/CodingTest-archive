d15ec533a43b77214f6f81182269260a714fac9f
Youngwoo Kwon <63556795+kwonup@users.noreply.github.com>
2025-09-04 11:48:14 +0900

    [level 1] Title: 폰켓몬, Time: 0.69 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,5 +1,6 @@
 def solution(nums):
     answer = 0
     unique_nums = set(nums)
-    answer = len(nums)/2 if len(nums)/2< len(unique_nums) else len(unique_nums)
+    #answer = len(nums)/2 if len(nums)/2< len(unique_nums) else len(unique_nums)
+    answer = min(len(nums)/2,len(unique_nums))
     return answer
